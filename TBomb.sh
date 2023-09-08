10a41b944c6de9935598954da7c1bbd875153cfa
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2023-09-08 20:19:21 +0530

    Update TBomb.sh

--- TBomb.sh
+++ TBomb.sh
@@ -27,21 +27,20 @@ banner() {
     clear
     echo -e "\e[1;31m"
     if ! [ -x "$(command -v figlet)" ]; then
-        echo 'Introducing TBomb'
+        echo 'Introducing e-Bomb'
     else
-        figlet TBomb
+        figlet e-Bomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;34m This Bomber Was Modified By \e[1;32mthetechboy3 \e[0m"
     else
-        echo -e "\e[1;34mCreated By \e[1;34m"
-        toilet -f mono12 -F border SpeedX
+        echo -e "\e[1;34mModified By \e[1;34m"
+        toilet -f mono12 -F border thetechboy3
     fi
-    echo -e "\e[1;34m For Any Queries Join Me!!!\e[0m"
-    echo -e "\e[1;32m           Telegram: https://t.me/TBombChat \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/SpeedXTech \e[0m"
+    echo -e "\e[1;34m For Any Queries Ping Me!!!\e[0m"
+    echo -e "\e[1;32m    Telegram: https://t.me/thetechboy3 \e[0m"
     echo " "
-    echo "NOTE: Kindly move to the PIP version Of TBomb for more stability."
+    echo "NOTE: Kindly move to the PIP version Of e-Bomb for more stability."
     echo " "
 }
 
@@ -127,7 +126,7 @@ do
         echo -e "\e[1;34m Downloading Latest Files..."
         rm -f .update
         $PYTHON bomber.py --update
-        echo -e "\e[1;34m RUN TBomb Again..."
+        echo -e "\e[1;34m RUN e-Bomb Again..."
         pause
         exit
     elif [ $ch -eq 5 ];then
